a8e2089c3c017b3d36f6ac0ec068184510f3b6c5
Oniya Daniel <daniel.prince84@gmail.com>
2022-09-19 19:16:13 +0100

    exercise concluded on structs

--- lectures/exercise/structs/structs.go
+++ lectures/exercise/structs/structs.go
@@ -17,8 +17,50 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type Coordinate struct {
+	x, y int
+}
+type Rectangle struct {
+	a Coordinate
+	b Coordinate
+}
+
+func width(r Rectangle) int {
+	return int(math.Abs(float64(r.b.x - r.a.x)))
+}
+
+func height(r Rectangle) int {
+	return int(math.Abs(float64(r.b.y - r.a.y)))
+}
+
+func area(r Rectangle) int {
+	return width(r) * height(r)
+}
+
+func perimeter(r Rectangle) int {
+	return (width(r) + height(r)) * 2
+}
+
+func printInfo(r Rectangle) {
+	fmt.Println("Rectangle,", r)
+	fmt.Println("Area is", area(r))
+	fmt.Println("Perimeter is", perimeter(r))
+}
 
 func main() {
+	rect := Rectangle{
+		a: Coordinate{0, 8},
+		b: Coordinate{7, 0},
+	}
+	printInfo(rect)
 
+	fmt.Println("- - - - - - Doubling Rectangle Size - - - - - - - -")
+	rect.b.x *= 2
+	rect.a.y *= 2
+	printInfo(rect)
 }
