a6db2187fe79a603266a94b6c10e22b676ed83f2
Oniya Daniel <daniel.prince84@gmail.com>
2022-09-20 20:55:33 +0100

    completed demo on maps

--- lectures/demo/maps/maps.go
+++ lectures/demo/maps/maps.go
@@ -3,4 +3,31 @@ package main
 import "fmt"
 
 func main() {
+	shoppingLists := make(map[string]int)
+	shoppingLists["eggs"] = 11
+	shoppingLists["milk"] = 1
+	shoppingLists["bread"] += 1
+
+	shoppingLists["eggs"] += 1
+	fmt.Println(shoppingLists)
+
+	delete(shoppingLists, "milk")
+	fmt.Println("Milk deleted, new list:", shoppingLists)
+
+	fmt.Println("Need", shoppingLists["eggs"], "eggs")
+
+	cereal, found := shoppingLists["cereal"]
+	fmt.Println("Need cereal?")
+	if !found {
+		fmt.Println("    Nope, cereal not on list")
+	} else {
+		fmt.Println("    Yup", cereal, "boxes")
+	}
+
+	totalItems := 0
+	for _, amount := range shoppingLists {
+		totalItems += amount
+	}
+	fmt.Println("There are", totalItems, "items on the shopping list")
+
 }
