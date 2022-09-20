b6a1f5dc6e7d64410252523a90a9ef2e96a2ce2b
Oniya Daniel <daniel.prince84@gmail.com>
2022-09-20 16:25:37 +0100

    done with array exercise

--- lectures/exercise/arrays/arrays.go
+++ lectures/exercise/arrays/arrays.go
@@ -18,6 +18,38 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	name  string
+	price float64
+}
 
+type ShoppingList [4]Product
+
+func (sL ShoppingList) printStat() {
+	var totalCost float64
+	var totalItems int
+
+	for i := 0; i < len(sL); i++ {
+		if sL[i].name == "" {
+			continue
+		}
+		totalItems++
+		totalCost += sL[i].price
+	}
+
+	fmt.Println("Last Item", sL[totalItems-1])
+	fmt.Println("Total number of items", totalItems)
+	fmt.Println("Total cost of items", totalCost)
+}
+
+func main() {
+	var shoppingList ShoppingList
+	shoppingList[0] = Product{name: "Cassava", price: 13.99}
+	shoppingList[1] = Product{name: "Garri", price: 14.99}
+	shoppingList[2] = Product{name: "Ewa", price: 22.99}
+	shoppingList.printStat()
+
+	fmt.Println(" - - - Adding one more item - - ")
+	shoppingList[3] = Product{name: "Agbado", price: 2.99}
+	shoppingList.printStat()
 }
