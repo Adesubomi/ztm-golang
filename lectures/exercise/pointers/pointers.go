665e1e8c29ac078f8d4be4c1196241b2c993c4f4
Oniya Daniel <daniel.prince84@gmail.com>
2022-09-20 23:34:05 +0100

    done with exercise on pointers

--- lectures/exercise/pointers/pointers.go
+++ lectures/exercise/pointers/pointers.go
@@ -18,6 +18,57 @@ package main
 
 import "fmt"
 
+const (
+	Active   = true
+	Inactive = false
+)
+
+type SecurityTag bool
+
+type Item struct {
+	name  string
+	state SecurityTag
+}
+
+func activateTag(state *SecurityTag) {
+	*state = Active
+}
+
+func deactivateTag(state *SecurityTag) {
+	*state = Inactive
+}
+
+func checkout(items []Item) {
+	fmt.Println("checking out...")
+	for i := 0; i < len(items); i++ {
+		deactivateTag(&items[i].state)
+	}
+}
+
 func main() {
 
+	//  - Create at least 4 items, all with active security tags
+	antelope := Item{name: "Antelope", state: Active}
+	dove := Item{name: "Dove", state: Active}
+	lion := Item{name: "Lion", state: Active}
+	platypus := Item{name: "Platypus", state: Active}
+
+	//  - Store them in a slice or array
+	items := []Item{antelope, dove, lion, platypus}
+	fmt.Println(items)
+
+	//  - Deactivate any one security tag in the array/slice
+	deactivateTag(&items[0].state)
+	deactivateTag(&items[2].state)
+	fmt.Println(items)
+
+	// oversabi
+	activateTag(&items[2].state)
+	fmt.Println(items)
+
+	//  - Call the checkout() function to deactivate all tags
+	checkout(items)
+	fmt.Println(items)
+
+	//  - Print out the array/slice after each change
 }
