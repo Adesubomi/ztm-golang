75cc0dca72fa7060f5ce05bc0d85e4ed59c80e55
Oniya Daniel <daniel.prince84@gmail.com>
2022-09-21 01:43:26 +0100

    section review - library (completed)

--- lectures/exercise/sr-library/library.go
+++ lectures/exercise/sr-library/library.go
@@ -19,8 +19,137 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Title string
+type Name string
+
+type LendAudit struct {
+	checkOut time.Time
+	checkIn  time.Time
+}
+
+type Member struct {
+	name  Name
+	books map[Title]LendAudit
+}
+
+type BookEntry struct {
+	total  int
+	lended int
+}
+
+type Liberary struct {
+	members map[Name]Member
+	books   map[Title]BookEntry
+}
+
+func printMemberAudit(member *Member) {
+	for title, audit := range member.books {
+		var returnTime string
+		if audit.checkIn.IsZero() {
+			returnTime = "[not returned yet]"
+		} else {
+			returnTime = audit.checkIn.String()
+		}
+		fmt.Println(member.name, ":", title, ":", audit.checkOut.String(), "through", returnTime)
+	}
+}
+
+func printMemberAudits(lib *Liberary) {
+	for _, member := range lib.members {
+		printMemberAudit(&member)
+	}
+}
+
+func printLiberaryBooks(lib *Liberary) {
+	fmt.Println()
+	for title, book := range lib.books {
+		fmt.Println(title, "/ total: ", book.total, "/ lended:", book.lended)
+	}
+	fmt.Println()
+}
+
+func checkoutBook(lib *Liberary, title Title, member *Member) bool {
+	book, found := lib.books[title]
+	if !found {
+		fmt.Println("Book not part of library")
+		return false
+	} else if book.lended >= book.total {
+		fmt.Println("No more books available to lend")
+		return false
+	}
+
+	book.lended += 1
+	lib.books[title] = book
+
+	member.books[title] = LendAudit{checkOut: time.Now()}
+	return true
+}
+
+func returnBook(lib *Liberary, title Title, member *Member) bool {
+	book, found := lib.books[title]
+	if !found {
+		fmt.Println("Book not part of library")
+		return false
+	}
+
+	audit, found := member.books[title]
+	if !found {
+		fmt.Println("Member did not check out this book")
+		return false
+	}
+
+	book.lended -= 1
+	lib.books[title] = book
+
+	audit.checkIn = time.Now()
+	member.books[title] = audit
+	return true
+}
 
 func main() {
 
+	library := Liberary{
+		books:   make(map[Title]BookEntry),
+		members: make(map[Name]Member),
+	}
+
+	library.books["Webapps in Go"] = BookEntry{
+		total: 4, lended: 0,
+	}
+	library.books["The Lttle Go Book"] = BookEntry{
+		total: 3, lended: 0,
+	}
+	library.books["Learning Go"] = BookEntry{
+		total: 2, lended: 0,
+	}
+	library.books["Go Bootcamp"] = BookEntry{
+		total: 1, lended: 0,
+	}
+
+	library.members["Jayson"] = Member{"Jayson", map[Title]LendAudit{}}
+	library.members["Billy"] = Member{"Billy", map[Title]LendAudit{}}
+	library.members["Susanna"] = Member{"Susanna", map[Title]LendAudit{}}
+
+	fmt.Println("\nInitial:")
+	printLiberaryBooks(&library)
+	printMemberAudits(&library)
+
+	member := library.members["Jayson"]
+	didCheckOut := checkoutBook(&library, "Go Bootcamp", &member)
+	fmt.Println("\nCheck out a book")
+	if didCheckOut {
+		printLiberaryBooks(&library)
+		printMemberAudits(&library)
+	}
+
+	didReturn := returnBook(&library, "Go Bootcamp", &member)
+	if didReturn {
+		printLiberaryBooks(&library)
+		printMemberAudits(&library)
+	}
 }
