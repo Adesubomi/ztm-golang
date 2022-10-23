a8436a2cae29942870ca7e1ac03e4564b56aeddd
Oniya Daniel <daniel.prince84@gmail.com>
2022-10-23 13:50:08 +0100

    receiver func exercise done

--- lectures/exercise/rcv-func/rcv-func.go
+++ lectures/exercise/rcv-func/rcv-func.go
@@ -16,6 +16,72 @@ package main
 
 import "fmt"
 
-func main() {
+type Player struct {
+	Name              string
+	Health, MaxHealth uint
+	Energy            uint
+	MaxEnergy         uint
+}
 
+func (p *Player) addHealth(amount uint) {
+	p.Health += amount
+	if p.Health > p.MaxHealth {
+		p.Health = p.MaxHealth
+	}
+
+	fmt.Println("Health added [", amount, "]")
+	fmt.Println("    New Health:", p.Health)
+}
+
+func (p *Player) applyDamage(amount uint) {
+	if amount > p.Health {
+		p.Health = 0
+	} else {
+		p.Health -= amount
+	}
+
+	fmt.Println("Health expended [", amount, "]")
+	fmt.Println("    New Health:", p.Health)
+}
+
+func (p *Player) addEnergy(amount uint) {
+	p.Energy += amount
+	if p.Energy > p.MaxEnergy {
+		p.Energy = p.MaxEnergy
+	}
+
+	fmt.Println("Energy added [", amount, "]")
+	fmt.Println("    New Energy:", p.Energy)
+}
+
+func (p *Player) consumeEnergy(amount uint) {
+	if amount > p.Energy {
+		p.Energy = 0
+	} else {
+		p.Energy -= amount
+	}
+
+	fmt.Println("Energy expended [", amount, "]")
+	fmt.Println("    New Energy:", p.Energy)
+}
+
+func main() {
+	player := Player{"Daniel", 100, 100, 75, 100}
+
+	player.applyDamage(10)
+	player.addEnergy(5)
+	player.consumeEnergy(5)
+	player.consumeEnergy(5)
+	player.consumeEnergy(5)
+	player.consumeEnergy(5)
+	player.consumeEnergy(5)
+	player.consumeEnergy(5)
+	player.consumeEnergy(5)
+	player.consumeEnergy(5)
+	player.consumeEnergy(5)
+	player.consumeEnergy(5)
+	player.consumeEnergy(5)
+	player.addEnergy(40)
+	player.addHealth(15)
+	player.consumeEnergy(999)
 }
